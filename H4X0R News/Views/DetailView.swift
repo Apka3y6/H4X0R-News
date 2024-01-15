a33165fb48ba732bd56b068acdf7fa5408d1f42e
Apka3y6 <vilgaukd@gmail.com>
2024-01-15 14:41:28 +0500

    structure change

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,40 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Dmitrii Vilgauk on 16.01.2024.
+//
+
+import SwiftUI
+//import WebKit
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+#Preview {
+    DetailView(url: "https://www.google.com")
+}
+
+//struct WebView: UIViewRepresentable {
+//    
+//    let urlString: String?
+//    
+//    func makeUIView(context: Context) -> WebView.UIViewType {
+//        return WKWebView()
+//    }
+//    
+//    func updateUIView(_ uiView: WKWebView, context: Context) {
+//        if let safeString = urlString {
+//            if let url = URL(string: safeString) {
+//                let request = URLRequest(url: url)
+//                uiView.load(request)
+//            }
+//        }
+//    }
+//}
